e66391670e4901c0d0b03559494274b716ccc113
Phil Skaggs <prskaggsjr@gmail.com>
2016-02-11 19:15:38 -0700

    Finished

--- test/test.js
+++ test/test.js
@@ -1,6 +1,14 @@
 var code = require('../main');
 var expect = require('chai').expect;
 
-describe('', function() {
-  it('');
+describe('Leap Year', function() {
+  it('Should be true for years / 4 || 400', function() {
+    expect(code.leapYear( 2016 )).to.equal(true);
+  })
+});
+
+describe('Leap Year', function() {
+  it('Should be false for years / 100 && not 400', function() {
+    expect(code.leapYear( 1700 )).to.equal(false);
+  })
 });
